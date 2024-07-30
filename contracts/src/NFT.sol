dca4cd0f00e6ffad12135da85f44a0f41c17c469
jacopomanenti01 <jacopomanenti01@gmail.com>
2024-07-30 22:15:34 +0200

    ethers.js config & first trial ok

--- contracts/src/NFT.sol
+++ contracts/src/NFT.sol
@@ -98,6 +98,14 @@ contract NFT is ERC1155Supply, AccessControl {
        return super.supportsInterface(_interfaceId);
     }
 
+    function getRoles(address account) public view returns (bool isRecordCompany, bool isWe, bool isAdmin) {
+        isRecordCompany = hasRole(RECORD_COMPANY_ROLE, account);
+        isWe = hasRole(WE_ROLE, account);
+        isAdmin = hasRole(DEFAULT_ADMIN_ROLE, account);
+        return (isRecordCompany, isWe, isAdmin);
+
+    }
+
     // function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal override(ERC1155, ERC1155Supply) {
     //     super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     // }
