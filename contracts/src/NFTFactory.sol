dd824564f9f333e88a487c7f0c81bc9e0d6e2e7b
ecivini <emanuele.civini@proton.me>
2024-08-02 09:54:53 +0200

    Add tests for NFTFactory

--- contracts/src/NFTFactory.sol
+++ contracts/src/NFTFactory.sol
@@ -16,12 +16,11 @@ contract NFTFactory is AccessControl, INFTFactory {
 
     /**
      * @dev Deploy a new NFT contract for a record company.
-     * @param _uri Base uri
      * @param _recordCompanyAdmin Initial record company admin address
      * @param _treasury Record company treasury
      */
-    function deployNFT(string memory _uri, address _recordCompanyAdmin, address _treasury) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        NFT nft = new NFT(_uri, _recordCompanyAdmin, _treasury);
+    function deployNFT(address _recordCompanyAdmin, address _treasury) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        NFT nft = new NFT(_recordCompanyAdmin, _treasury);
 
         isFactoryDeployed[address(nft)] = true;
         associatedNFT[_recordCompanyAdmin] = address(nft);
--- contracts/test/NFTFactory.t.sol
+++ contracts/test/NFTFactory.t.sol
@@ -6,22 +6,27 @@ import {NFT} from "../src/NFT.sol";
 import {NFTFactory} from "../src/NFTFactory.sol";
 
 contract NFTFactoryTest is Test {
+    event RoleGranted(bytes32 indexed role, address indexed account, address indexed sender);
+    event NewNFT(address indexed _nft, address indexed _initialAdmin);
+    
     NFTFactory public factory;
     NFT public nft;
 
     address public recordCompanyAdmin = address(0x1);
     address public treasury = address(0x2);
 
-    string public baseUri = "https://.../";
-
     function setUp() public {
         factory = new NFTFactory();
-        factory.deployNFT(baseUri, recordCompanyAdmin, treasury);
-
-        nft = NFT(factory.associatedNFT(recordCompanyAdmin));
     }
 
-    function test_Deployment() public view {
+    function test_Deployment() public {
+        // Deploy new contract
+        factory.deployNFT(recordCompanyAdmin, treasury);
+
+        // Save nft object for later tests
+        nft = NFT(factory.associatedNFT(recordCompanyAdmin));
+
+        assertNotEq(address(nft), address(0), "Contract not deployed");
         assertEq(nft.hasRole(nft.RECORD_COMPANY_ROLE(), recordCompanyAdmin), true, "Record company admin misses role");
         assertEq(nft.hasRole(nft.DEFAULT_ADMIN_ROLE(), msg.sender), true, "Owner admin misses role");
     }
