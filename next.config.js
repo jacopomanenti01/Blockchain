32c9f6258872e218a51930791c95667f8346ef23
jacopomanenti01 <jacopomanenti01@gmail.com>
2024-08-10 14:50:23 +0200

    fixed npm run build bug

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // If serverActions needs to be enabled and it's configurable as a boolean in your version,
@@ -17,9 +19,14 @@ const nextConfig = {
   },
   reactStrictMode: true,
   webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'components'); // Adatta questo al tuo progetto
+    return config;
+  },
+  webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
+
 };
 
 module.exports = nextConfig;
